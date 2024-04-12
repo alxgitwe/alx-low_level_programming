abc6f96c06c397038f3274194e1d7e38b505d069
alxgitwe <el.najib15@gmail.com>
2024-04-12 15:57:53 -0700

    m

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -2,12 +2,12 @@
 
 /**
  * hash_table_create - hash table
- * @size: size
+ * @x: size
  *
  * Return: return
  */
 
-hash_table_t *hash_table_create(unsigned long int size)
+hash_table_t *hash_table_create(unsigned long int x)
 {
 	hash_table_t *y;
 	unsigned long int j;
@@ -17,17 +17,16 @@ hash_table_t *hash_table_create(unsigned long int size)
 	if (y == NULL)
 		return (NULL);
 
-	y->size = size;
+	y->size = x;
 
-	y->array = malloc(sizeof(hash_node_t *) * size);
+	y->array = malloc(sizeof(hash_node_t *) * x);
 
 	if (y->array == NULL)
 	{
-		free(y);
 		return (NULL);
 	}
 	
-	for (j = 0; j < size; j++)
+	for (j = 0; j < x; j++)
 		y->array[j] = NULL;
 
 	return (y);
