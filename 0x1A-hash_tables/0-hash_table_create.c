4dfe820c2799e7cdf65225161840740aafd1de91
alxgitwe <el.najib15@gmail.com>
2024-04-12 17:04:00 -0700

    m

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -2,34 +2,25 @@
 
 /**
  * hash_table_create - hash table
- *
- * @x: size
+ * @size: pointer
  *
  * Return: return
  */
-
-hash_table_t *hash_table_create(unsigned long int x)
+hash_table_t *hash_table_create(unsigned long int size)
 {
-	hash_table_t *y;
+	hash_table_t *a;
+	unsigned long int b;
 
-	unsigned long int j;
-
-	y = malloc(sizeof(hash_table_t));
-
-	if (y == NULL)
+	a = malloc(sizeof(hash_table_t));
+	if (a == NULL)
 		return (NULL);
 
-	y->size = x;
-
-	y->array = malloc(sizeof(hash_node_t *) * x);
-
-	if (y->array == NULL)
-	{
+	a->size = size;
+	a->array = malloc(sizeof(hash_node_t *) * size);
+	if (a->array == NULL)
 		return (NULL);
-	}
-	
-	for (j = 0; j < x; j++)
-		y->array[j] = NULL;
+	for (b = 0; b < size; b++)
+		a->array[b] = NULL;
 
-	return (y);
+	return (a);
 }
